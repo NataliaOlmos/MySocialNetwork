c80d4d0b5c180c60eacdf9d9bd9d138e5275c0d1
Natalia <nataliav_4428@hotmail.com>
2020-12-11 22:51:19 -0600

    styles

--- Routes/postIt.js
+++ Routes/postIt.js
@@ -6,9 +6,11 @@ function renderPost() {
    <input  class="card-img-top img-fluid" alt="Card image cap" id="file" type="file" accept="image/" multiple>
    <textarea class="textPost" id="textPost" placeholder="Actualiza tu perfil" type="text" cols="30" rows="10"></textarea>
    <button id="postBtn" class="up">Add Post</button>
+   <h3>Preview de tu post</h3>
    <div id="results">
    <figure> 
    </figure>
+  
    </div>
    </div>
    `
@@ -17,7 +19,6 @@ function renderPost() {
   let addBtn = document.querySelector("#postBtn");
     let text = document.querySelector("#textPost");
     let fileInput = document.querySelector("#file");
-    let image = document.getElementById('preview');
     let resultsContainer = document.getElementById('results');
     const btnDelete = document.querySelector("#deletePost");
     const todayIs = new Date();
@@ -73,22 +74,24 @@ postsRef.onSnapshot(function(snap){
    
 
 function drawResults(array){
-  resultsContainer.innerHTML = ""
+  //resultsContainer.innerHTML = ""
   array.forEach(function(el, index, body, id, dateValue){
         let figure = document.createElement('figure')
+        
         figure.innerHTML = `
+        
          <div class="resultsWrapper">
-            <img src="${el.link}" alt="${index}">
-            <p>${el.body}</p>
-            <p>${el.date}</p>
-            <button id="deletePost" >Borrar</button>
-            <button id="edit" >Editar</button> 
-            </div>
+            <img id="imgPost" src="${el.link}" alt="${index}">
+            <p>Descripción:${el.body}</p>
+            <p>Subido el:<span class="minHeight collapse" id="collapseExample">${el.date.slice(0, -32)}</span></p>
+         </div>
+        <div id="goTo">
+         <a href="#" target="_blank">Ir a Mi Perfil</a>
+        </div> 
             `
         resultsContainer.prepend(figure)
 
 
-       
       
       
        
